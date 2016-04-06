c6a09b8fb1a872eea5e79372b61d3639d804eea5
henry <henry8812@gmail.com>
2016-04-05 21:53:07 -0500

    [UPDATE]

--- index.js
+++ index.js
@@ -25,7 +25,11 @@ app.listen(app.get('port'), function() {
 main = require("./main");
 app = main.getServer();
 app.set('port', (process.env.PORT || 5000));
+
+app.get("/", function(req, res){
+    res.send("hello");
+})
 app.listen(app.get("port"), function(){
     
-    console.log('Node app is running on port', app.get('port'));
+        console.log('Node app is running on port', app.get('port'));
 });
