12ccb8270d3d0839f4477bee2d8f76680ddd78c8
henry <henry8812@gmail.com>
2016-04-07 10:46:05 -0500

    [NODE]-init

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ settings.use(app);
 app.set('view engine', 'ejs');
 account.set(app, public_html);
 app.set('port', (process.env.PORT || 5000));
+
 app.get("/", function(req, res){
     res.render("pages/index", {status:"logged"});
 })
+
+
 server.listen(app.get("port"), function(){
     console.log('Node app is running on port', app.get('port'));
 });
